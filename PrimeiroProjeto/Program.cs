4cba1b7a22b4e3fb115b90e57541ce4611caf58a
Bruno Mota <bmotadev@gmail.com>
2022-12-08 13:19:16 -0300

    Adiciona primeiro projeto do curso de POO

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+        
+    }
+}
