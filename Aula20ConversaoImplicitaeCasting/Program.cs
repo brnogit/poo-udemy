62009be9442e8dd86a7820188833e66842bacff2
Bruno Mota <bmotadev@gmail.com>
2022-12-09 18:28:53 -0300

    aula 20 - conversao implicita e casting

---
+++ Aula20ConversaoImplicitaeCasting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double a;
+            //int b;
+
+            //a = 5.1;
+            //b = (int)a; 
+
+            //Console.WriteLine(b);
+
+            int a = 5;
+            int b = 2;
+
+            double resultado = (double) a / b;
+
+            Console.WriteLine(resultado);
+
+            Console.ReadLine();
+        }
+    }
+}
