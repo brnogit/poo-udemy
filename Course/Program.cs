159cd2afe9a44424ad0d012c2e782bab53b7db26
Bruno Mota <bmotadev@gmail.com>
2022-12-09 12:23:57 -0300

    utilizando double, float e string

--- Course/Program.cs
+++ Course/Program.cs
@@ -13,16 +13,22 @@ namespace Course
             int n2 = 1000;
             int n3 = 2147483647;
             long n4 = 2147483648L; //L é pra indicar explicitamente que é um numero long
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            string nome = "Maria Green";
 
             n1++; //extrapola o limite e vai pra 0
 
             Console.WriteLine(completo);
+            Console.WriteLine(nome);
             Console.WriteLine(genero);
             Console.WriteLine(letra);
             Console.WriteLine(n1);
             Console.WriteLine(n2);
             Console.WriteLine(n3);
             Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
             Console.ReadLine();
         }
     }
