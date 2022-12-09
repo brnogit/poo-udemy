bd41f293a0bb082529d045dd0c273bc50d9c817f
Bruno Mota <bmotadev@gmail.com>
2022-12-09 18:18:22 -0300

    Aula 19 operadores de atribuicao

---
+++ Aula19OperadoresDeAtribuicao/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+
+            a += 2;
+            Console.WriteLine(a);
+
+            a *= 3;
+            Console.WriteLine(a);
+
+            string s = "ABC";
+            Console.WriteLine(s);
+
+            s += "DEF";
+            Console.WriteLine(s);
+
+            int valorA = 10;
+            //int valorB = valorA++;
+            int valorB = ++valorA;
+            Console.WriteLine(valorA);
+            Console.WriteLine(valorB);
+
+            Console.ReadLine();
+        }
+    }
+}
