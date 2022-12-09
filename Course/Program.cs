4baef92b39d7116617844ee65b7f685817afd4f0
Bruno Mota <bmotadev@gmail.com>
2022-12-09 12:04:25 -0300

    utilizando tipo long e int

--- Course/Program.cs
+++ Course/Program.cs
@@ -7,10 +7,16 @@ namespace Course
         static void Main(string[] args)
         {
             byte n1 = 255;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L; //L é pra indicar explicitamente que é um numero long
 
             n1++; //extrapola o limite e vai pra 0
 
             Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
             Console.ReadLine();
         }
     }
