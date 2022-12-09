6c54703dffae8a5c91e0696c4a2861ecf43a0cd8
Bruno Mota <bmotadev@gmail.com>
2022-12-09 16:03:01 -0300

    diferencas entre write e writeline

---
+++ Aula18SaidaDeDados/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bom dia!");
+            Console.WriteLine("Boa noite!");
+            Console.WriteLine("Boa tarde!");
+        }
+    }
+}
