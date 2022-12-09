0eb42a79e68bff4fb053a054b9f58ee26044f0b8
Bruno Mota <bmotadev@gmail.com>
2022-12-09 12:21:52 -0300

    utilizando unicode pelo codigo

--- Course/Program.cs
+++ Course/Program.cs
@@ -8,6 +8,7 @@ namespace Course
         {
             bool completo = false;
             char genero = 'f';
+            char letra = '\u0041';
             byte n1 = 255;
             int n2 = 1000;
             int n3 = 2147483647;
@@ -17,6 +18,7 @@ namespace Course
 
             Console.WriteLine(completo);
             Console.WriteLine(genero);
+            Console.WriteLine(letra);
             Console.WriteLine(n1);
             Console.WriteLine(n2);
             Console.WriteLine(n3);
