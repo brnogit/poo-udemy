52637b3ab45aaf950131a45682adaeed66cac0f9
Bruno Mota <bmotadev@gmail.com>
2022-12-13 15:18:02 -0300

    Aula 27 Estrutura Condicional - Primeiro Exemplo

---
+++ Aula27EstruturaCondicional/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            Console.WriteLine("Bom dia");
+
+            if(x > 5 )
+            {
+                Console.WriteLine("Boa tarde");
+            }
+            
+            Console.WriteLine("Boa noite");
+
+            Console.ReadLine();
+        }
+    }
+}
