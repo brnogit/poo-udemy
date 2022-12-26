f6942ecbaa4beb4834bcd2e861e229a0d43ec297
Bruno Mota <bmotadev@gmail.com>
2022-12-26 18:38:59 -0300

    Aula 32 - Estrutura Repetitiva Enquanto - WHILE - Concluida

--- Aula32EstruturaRepetitivaWhile/Program.cs
+++ Aula32EstruturaRepetitivaWhile/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Course
 {
@@ -6,7 +7,20 @@ namespace Course
     {
         static void Main(string[] args)
         {
+            
+            Console.Write("Digite um número: ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+            while ( x >= 0.0)
+            {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+                Console.Write("Digite outro número: ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            Console.WriteLine("Número negativo!");
+            Console.ReadLine();
         }
     }
 }
