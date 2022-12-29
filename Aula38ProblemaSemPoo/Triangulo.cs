fb4e5072ab1436ad20a0fb4790bccb07491fdec2
Bruno Mota <bmotadev@gmail.com>
2022-12-29 16:51:19 -0300

    Aula 41 Criando um método

--- Aula38ProblemaSemPoo/Triangulo.cs
+++ Aula38ProblemaSemPoo/Triangulo.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 namespace Course
 {
     class Triangulo
@@ -6,5 +7,11 @@ namespace Course
         public double A;
         public double B;
         public double C;
+
+        public double Area() {
+            double p = (A + B + C) / 2.0;
+            double raiz = Math.Sqrt(p * (p - A) * ( p - B) * (p - C));
+            return raiz;
+        }
     }
 }
