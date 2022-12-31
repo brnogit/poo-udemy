2b08bbc371e6ff6585938ddf8b69684366e2abf2
Bruno Mota <bmotadev@gmail.com>
2022-12-30 23:17:52 -0300

    Aula 43 Object e ToString

--- Aula42SegundoProblemExemplo/Produto.cs
+++ Aula42SegundoProblemExemplo/Produto.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Aula42SegundoProblemExemplo
 {
@@ -15,6 +12,11 @@ namespace Aula42SegundoProblemExemplo
         public double ValorTotalEmEstoque()
         {
             return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: $ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
     }
 }
