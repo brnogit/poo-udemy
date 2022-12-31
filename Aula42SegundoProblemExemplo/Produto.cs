1140e8bcf4625890df8c2b953b69b0765285b3de
Bruno Mota <bmotadev@gmail.com>
2022-12-31 20:50:48 -0300

    Aula 44 Terminando de construir o programa

--- Aula42SegundoProblemExemplo/Produto.cs
+++ Aula42SegundoProblemExemplo/Produto.cs
@@ -14,6 +14,16 @@ namespace Aula42SegundoProblemExemplo
             return Preco * Quantidade;
         }
 
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
         public override string ToString()
         {
             return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: $ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
--- Aula42SegundoProblemExemplo/Program.cs
+++ Aula42SegundoProblemExemplo/Program.cs
@@ -19,7 +19,24 @@ namespace Course
             Console.Write("Quantidade no estoque: ");
             produto.Quantidade = int.Parse(Console.ReadLine());
 
+            Console.WriteLine();
             Console.WriteLine("Dados do produto: " + produto);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            produto.AdicionarProdutos(quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + produto);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            quantidade = int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + produto);
         }
     }
 }
