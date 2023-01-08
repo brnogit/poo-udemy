45c20cd65eef82cdf5127c03bc4c2947c91ca670
Bruno Mota <bmotadev@gmail.com>
2023-01-07 21:42:13 -0300

    Aula 48 Membros Estaticos - Preparacao do ambiente

--- Aula46MembrosEstaticos/Program.cs
+++ Aula46MembrosEstaticos/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
